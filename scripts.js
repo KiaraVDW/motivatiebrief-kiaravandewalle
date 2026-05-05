8ad5361f1faadffbc8c153e4eb95074cc10653b8
KiaraVDW <kiara.vandewalle@student.vives.be>
2026-05-05 08:14:02 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -101,19 +101,3 @@ document.addEventListener("DOMContentLoaded", () => {
         dots[0].classList.add("active");
     });
 });
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        const target = document.querySelector(this.getAttribute("href"));
-        if (!target) return;
-
-        const offset = 90; // hoogte van je navbar
-        const top = target.getBoundingClientRect().top + window.scrollY - offset;
-
-        window.scrollTo({
-            top: top,
-            behavior: "smooth"
-        });
-    });
-});
